a0d0a2ad66c59dabe3820451523335d8038e7e70
Bryant Taylor <btaylor5@uw.edu>
2016-09-29 19:57:55 -0700

    No idea where I was in this tutorial... Assuming I wasn't learning something immediately relevant and skipped out

---
+++ content_bruter.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+
+import Queue
+import threading
+import urllib2
+import urllib
+
+threads = 50
+target_url = "http://www.vantagenews.co"
+wordlist_file = "./all.txt"
+resume = None
+user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0"
+
+def build_wordlist(wordlist_file):
+    fd = open(wordlist_file, "rb")
+    raw_words = fd.readlines()
+    fd.close()
+
+    found_resume = False
+    words = Queue.Queue()
+
+    for word in raw_words:
+        print word
+        word = word.rstrip()
+        if resume is not None:
+            if found_resume:
+                words.put(word)
+            else:
+                if word == resume:
+                    found_resume = True
+                    print "Resuming wordlist from: %s" % resume
+        else:
+            words.put(word)
+    return words
+
+def dir_bruter(word_queue,extensions=None):
+    while not word_queue.empty():
+        attempt = word_queue.get()
+        attempt_list = []
+
+        if "." not in attempt:
+            attempt_list.append("/%s/" % attempt)
+        else:
+            attempt_list.append("/%s" % attempt)
+
+        if extensions:
+            for exten in extensions:
+                attempt_list.append("%s%s" % (attempt, exten))
+
+        for brute in attempt_list:
+            url = "%s%s" % (target_url, urllib.quote(brute))
+
+            try:
+                headers = {}
+                headers["User-Agent"] = user_agent
+                r = urllib2.Request(url, headers=headers)
+
+                response = urllib2.urlopen(r)
+
+                if len(response.read()):
+                    print "[%d] => %s" % (response.code, url)
+            except urllib2.URLError,e:
+                if hasattr(e, 'code') and e.code != 404:
+                    print "!!! %d => %s" % (e.code, url)
+                pass
+
+word_queue = build_wordlist(wordlist_file)
+extensions = [".php", ".html", ".bak", ".orig", ".inc", ".txt"]
+
+for i in xrange(threads):
+    t = threading.Thread(target=dir_bruter, args=(word_queue, extensions))
+    t.start()
---
+++ joomla_killer.py
@@ -0,0 +1,3 @@
+# NOTE: This example didn't offer many new concepts
+# Things it did teach -> Understand frameworks and how they prevent brute force
+# attacks. Often times we can still circumvent those techniques.
